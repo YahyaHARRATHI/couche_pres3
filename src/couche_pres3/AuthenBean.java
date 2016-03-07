a30a1b2766fc37c802c7fd00b89e23205b1bfbe2
yehyoun <yehyoun@live.fr>
2016-03-07 21:00:47 +0100

    GetList by name  and firstname
    we have also authentification bean and xhtml fle but doesn't work yet

---
+++ src/couche_pres3/AuthenBean.java
@@ -0,0 +1,68 @@
+package couche_pres3;
+
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpSession;
+
+import dal.beans.Client;
+import dal.services.SpringFactoryClient;
+
+@Named
+@SessionScoped
+//implement seriazable car sessionscoped
+public class AuthenBean implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String login;
+	private String password;
+	SpringFactoryClient factclient;
+	
+	//le client n'est pas un attribut car on va l'utiliser dans autre vue
+	//on va l'instancier et le mettre dans la session
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	public String connect(){
+		
+		 factclient=new SpringFactoryClient();
+		Client c= factclient.getClientService().authenticate(this.login, this.password);
+		
+		if(c==null){
+			
+		System.out.println("nullllllllllllllllllll");
+			return null;
+		}
+			
+		else  {
+			//ajout le client dans la session
+			//le client doit etre de porté session
+			//Recupération de facesContext
+			FacesContext context=FacesContext.getCurrentInstance();
+			//soit on l'ajout dans map
+			//ou
+			HttpSession session=(HttpSession) context.getExternalContext().getSession(true);
+			session.setAttribute("client", c);
+			
+		return "Authentification";
+	}
+	}
+
+}
---
+++ src/couche_pres3/ClientBean.java
@@ -0,0 +1,99 @@
+package couche_pres3;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.InvocationContext;
+
+import dal.beans.Client;
+import dal.beans.Compte;
+import dal.services.SpringFactoryClient;
+
+@Named
+@RequestScoped
+public class ClientBean {
+	private Logger logger;
+	private Long num;
+	private String nom;
+	private String prenom ;
+	
+	private Collection<Compte> comptes;
+	//pas la peine car depuis d'un jar  //pas distribu�
+	
+	SpringFactoryClient factclient;
+	
+	
+	
+	public Collection<Compte> getComptes() {
+		return comptes;
+	}
+	public void setComptes(Collection<Compte> comptes) {
+		this.comptes = comptes;
+	}
+	public void setComptes(List<Compte> comptes) {
+		this.comptes = comptes;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+	
+	public String listComptes(){
+		  
+		factclient=new SpringFactoryClient();
+		List<Client> client= new ArrayList<Client>();
+		client=factclient.getClientService().findClient(nom, prenom);
+		Client res=client.get(0);
+		/*for (Iterator iterator = client.iterator(); iterator.hasNext();) {
+			Client client2 = (Client) iterator.next();
+			this.comptes=client2.getComptes();
+			
+		}
+		*/
+		this.comptes=res.getComptes();
+		return null;
+		
+	}
+	
+	
+	public Long getNum() {
+		return num;
+	}
+	public void setNum(Long num) {
+		this.num = num;
+	}
+	
+	/*
+	
+	@AroundInvoke
+	private Object logMethod(InvocationContext ic) throws Exception {
+		logger.entering(ic.getTarget().toString(), ic.getMethod().getName());
+		 try {
+			return ic.proceed();
+		} finally  {
+			logger.exiting(ic.getTarget().toString(), ic.getMethod().getName());
+		}
+		 
+		 
+	}
+	*/
+	
+
+}
